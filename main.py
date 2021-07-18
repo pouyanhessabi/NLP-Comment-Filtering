6fa2b9e1069e29c94ece859ceab32f0564e8fc0e
pouyan.hessabi <pouyan.hessabi@gmail.com>
2021-07-19 01:30:49 +0430

    create hashmap(dictionary in python), key: word - value: count of word in each files

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+positive_hashmap = {}
+negative_hashmap = {}
+positive_lines = []
+negative_lines = []
+
 with open("rt-polarity.pos", "r") as file_positive:
     all_text_positive_file = file_positive.read()
 with open("rt-polarity.neg", "r") as file_negative:
@@ -14,11 +19,30 @@ def clean_text(text: str):
     text = text.replace('[', '')
     text = text.replace(']', '')
     text = text.replace('-', " ")
+    text = text.replace("  ", " ")
     return text
 
 
-# clean_negative = clean_and_pre_processing(all_text_negative_file)
-# print(clean_negative)
+def create_both_hashmap():
+    for line in positive_lines:
+        for word in line.split():
+            if word not in positive_hashmap:
+                positive_hashmap[word] = 1
+            else:
+                positive_hashmap[word] += 1
+
+    for line in negative_lines:
+        for word in line.split():
+            if word not in negative_hashmap:
+                negative_hashmap[word] = 1
+            else:
+                negative_hashmap[word] += 1
+
+
 all_text_positive_file = clean_text(all_text_positive_file)
 all_text_negative_file = clean_text(all_text_negative_file)
-print(all_text_negative_file)
+
+positive_lines = all_text_positive_file.splitlines()
+negative_lines = all_text_negative_file.splitlines()
+
+create_both_hashmap()
