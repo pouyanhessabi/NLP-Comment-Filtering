998e6aba60409527967f64c3c9ca4268ec7d04a1
pouyan.hessabi <pouyan.hessabi@gmail.com>
2021-07-19 23:17:48 +0430

    bigram problem with space fixed.

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ positive_lines_word_by_word = []
 negative_lines_word_by_word = []
 p_wi_and_next_word_in_positive = {}
 p_wi_and_next_word_in_negative = {}
+lambda1 = 0.15
+lambda2 = 0.7
+lambda3 = 0.15
+e = 0.1
 
 with open("rt-polarity.pos", "r") as file_positive:
     all_text_positive_file = file_positive.read()
@@ -83,7 +87,7 @@ def clean_both_hashmap():
     """""
     print("length of positive_hashmap after  cleaning:", len(positive_hashmap))
     print("length of negative_hashmap after  cleaning:", len(negative_hashmap))
-    
+
     print(deleted_words_positive)
     print(deleted_words_negative)
     """""
@@ -107,10 +111,7 @@ def calculate_probabilities_with_lambda():
     0 < e < 1
     e: shows accuracy if a word not in trained dictionary. 
     """""
-    lambda1 = 0.15
-    lambda2 = 0.7
-    lambda3 = 0.15
-    e = 0.1
+
     for key in bigram_matrix_positive:
         if (key.split()[0] in positive_hashmap) and (key.split()[0] not in deleted_words_positive) and \
                 (key.split()[1] not in deleted_words_positive):
@@ -124,84 +125,146 @@ def calculate_probabilities_with_lambda():
                                                   (lambda2 * p_wi_in_negative[key.split()[0]]) + (lambda1 * e)
 
 
-all_text_positive_file = clean_text(all_text_positive_file)
-all_text_negative_file = clean_text(all_text_negative_file)
-
-positive_lines = all_text_positive_file.splitlines()
-negative_lines = all_text_negative_file.splitlines()
-
-create_both_hashmap()
-clean_both_hashmap()
-
-# add <$> at the first of line and </$> at the end of line
-i = 0
-while i < len(positive_lines):
-    positive_lines[i] = "<$> " + positive_lines[i] + " </$>"
-    i += 1
-# add to hashmap
-positive_hashmap["<$>"] = i
-positive_hashmap["</$>"] = i
-
-i = 0
-while i < len(negative_lines):
-    negative_lines[i] = "<$> " + negative_lines[i] + " </$>"
-    i += 1
-# add to hashmap
-negative_hashmap["<$>"] = i
-negative_hashmap["</$>"] = i
-
-# probability of each word in language
-p_wi_in_positive = {}
-number_of_words_positive = sum(positive_hashmap.values())
-for key in positive_hashmap:
-    p_wi_in_positive[key] = positive_hashmap[key] / number_of_words_positive
-
-p_wi_in_negative = {}
-number_of_words_negative = sum(negative_hashmap.values())
-for key in negative_hashmap:
-    p_wi_in_negative[key] = negative_hashmap[key] / number_of_words_negative
-
-positive_lines_word_by_word = str(positive_lines).split()
-negative_lines_word_by_word = str(negative_lines).split()
-
-# make it clean with stop words
-# update_and_clean_lines()
-
-# build bigram matrices and save bigram count of each word
-bigram_matrix_positive = {}
-bigram_matrix_negative = {}
-
-i = 0
-while i < len(positive_lines_word_by_word) - 1:
-    key = str(positive_lines_word_by_word[i] + " " + positive_lines_word_by_word[i + 1] + " ")
-    if key not in bigram_matrix_positive:
-        bigram_matrix_positive[key] = 1
-    else:
-        bigram_matrix_positive[key] += 1
-    i += 1
-
-i = 0
-while i < len(negative_lines_word_by_word) - 1:
-    key = str(negative_lines_word_by_word[i] + " " + negative_lines_word_by_word[i + 1] + " ")
-    if key not in bigram_matrix_negative:
-        bigram_matrix_negative[key] = 1
-    else:
-        bigram_matrix_negative[key] += 1
-    i += 1
-
-# calculate probability of p(wi|w<i-1>)
-for key in bigram_matrix_positive:
-    if (key.split()[0] in positive_hashmap) and (key.split()[0] not in deleted_words_positive) and \
-            (key.split()[1] not in deleted_words_positive):
-        p_wi_and_next_word_in_positive[key] = bigram_matrix_positive[key] / positive_hashmap[key.split()[0]]
-for key in bigram_matrix_negative:
-    if (key.split()[0] in negative_hashmap) and (key.split()[0] not in deleted_words_negative) and \
-            (key.split()[1] not in deleted_words_negative):
-        p_wi_and_next_word_in_negative[key] = bigram_matrix_negative[key] / negative_hashmap[key.split()[0]]
-
-# considering lambda
-calculate_probabilities_with_lambda()
-
 input_string = input()
 while input_string != "!q":
+    all_text_positive_file = clean_text(all_text_positive_file)
+    all_text_negative_file = clean_text(all_text_negative_file)
+
+    positive_lines = all_text_positive_file.splitlines()
+    negative_lines = all_text_negative_file.splitlines()
+
+    create_both_hashmap()
+    clean_both_hashmap()
+
+    # add <$> at the first of line and </$> at the end of line
+    i = 0
+    while i < len(positive_lines):
+        positive_lines[i] = "<$> " + positive_lines[i] + " </$>"
+        i += 1
+    # add to hashmap
+    positive_hashmap["<$>"] = i
+    positive_hashmap["</$>"] = i
+
+    i = 0
+    while i < len(negative_lines):
+        negative_lines[i] = "<$> " + negative_lines[i] + " </$>"
+        i += 1
+    # add to hashmap
+    negative_hashmap["<$>"] = i
+    negative_hashmap["</$>"] = i
+
+    # probability of each word in language
+    p_wi_in_positive = {}
+    number_of_words_positive = sum(positive_hashmap.values())
+    for key in positive_hashmap:
+        p_wi_in_positive[key] = positive_hashmap[key] / number_of_words_positive
+
+    p_wi_in_negative = {}
+    number_of_words_negative = sum(negative_hashmap.values())
+    for key in negative_hashmap:
+        p_wi_in_negative[key] = negative_hashmap[key] / number_of_words_negative
+
+    positive_lines_word_by_word = str(positive_lines).split()
+    negative_lines_word_by_word = str(negative_lines).split()
+
+    # make it clean with stop words
+    # update_and_clean_lines()
+
+    # build bigram matrices and save bigram count of each word
+    bigram_matrix_positive = {}
+    bigram_matrix_negative = {}
+
+    i = 0
+    while i < len(positive_lines_word_by_word) - 1:
+        key = str(positive_lines_word_by_word[i] + " " + positive_lines_word_by_word[i + 1])
+        if key not in bigram_matrix_positive:
+            bigram_matrix_positive[key] = 1
+        else:
+            bigram_matrix_positive[key] += 1
+        i += 1
+
+    i = 0
+    while i < len(negative_lines_word_by_word) - 1:
+        key = str(negative_lines_word_by_word[i] + " " + negative_lines_word_by_word[i + 1] + " ")
+        if key not in bigram_matrix_negative:
+            bigram_matrix_negative[key] = 1
+        else:
+            bigram_matrix_negative[key] += 1
+        i += 1
+
+    # calculate probability of p(wi|w<i-1>)
+    for key in bigram_matrix_positive:
+        if (key.split()[0] in positive_hashmap) and (key.split()[0] not in deleted_words_positive) and \
+                (key.split()[1] not in deleted_words_positive):
+            p_wi_and_next_word_in_positive[key] = bigram_matrix_positive[key] / positive_hashmap[key.split()[0]]
+    for key in bigram_matrix_negative:
+        if (key.split()[0] in negative_hashmap) and (key.split()[0] not in deleted_words_negative) and \
+                (key.split()[1] not in deleted_words_negative):
+            p_wi_and_next_word_in_negative[key] = bigram_matrix_negative[key] / negative_hashmap[key.split()[0]]
+
+    # considering lambda
+    calculate_probabilities_with_lambda()
+
+    # main function
+    list_input = input_string.split()
+    list_input.insert(0, "<$>")
+    list_input.insert(len(list_input), "</$>")
+
+    deleted_from_input_positive = []
+    deleted_from_input_negative = []
+    for i in list_input:
+        if i not in positive_hashmap:
+            deleted_from_input_positive.append(i)
+    # clean input
+    for i in deleted_from_input_positive:
+        if i in list_input:
+            list_input.remove(i)
+
+    for i in list_input:
+        if i not in negative_hashmap:
+            deleted_from_input_negative.append(i)
+    # clean input
+    for i in deleted_from_input_negative:
+        if i in list_input:
+            list_input.remove(i)
+
+    print(list_input)
+    i = 1
+    for k in p_wi_and_next_word_in_positive:
+        print(k)
+        if k == "new conan ":
+            print("sag")
+        i += 1
+        if i > 10:
+            break
+    if "new conan" in p_wi_and_next_word_in_positive:
+        print("hast")
+
+    else:
+        print("nist")
+    exit()
+    p_multiplication_positive = p_wi_in_positive[list_input[0]]
+    i = 1
+    while i < len(list_input):
+        two_word = str(list_input[i - 1] + " " + list_input[i])
+        print(two_word)
+        if two_word in p_wi_and_next_word_in_positive:
+            print(p_multiplication_positive)
+            p_multiplication_positive *= p_wi_and_next_word_in_positive[two_word]
+        else:
+            p_multiplication_positive *= (lambda2 * p_wi_in_positive[list_input[i]]) + (lambda3 * e)
+        i += 1
+
+    i = 1
+    p_multiplication_negative = p_wi_in_negative[list_input[0]]
+    while i < len(list_input):
+        two_word = str(list_input[i - 1] + " " + list_input[i])
+        if two_word in p_wi_and_next_word_in_negative:
+            p_multiplication_negative *= p_wi_and_next_word_in_negative[two_word]
+        else:
+            p_multiplication_negative *= (lambda2 * p_wi_in_negative[list_input[i]]) + (lambda3 * e)
+        i += 1
+
+    print(p_multiplication_positive)
+    print(p_multiplication_negative)
     input_string = input()
