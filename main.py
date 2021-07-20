27d9eda2fe0cd0cdfbb992c2d147d8930742cd1c
pouyan.hessabi <pouyan.hessabi@gmail.com>
2021-07-20 12:51:39 +0430

    old code have big problem, that read whole document. clean code and affect design. before probability everything is ok.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+new_positive_lines = []
+new_negative_lines = []
 positive_hashmap = {}
 negative_hashmap = {}
 positive_lines = []
@@ -13,41 +15,64 @@ lambda2 = 0.15
 lambda3 = 0.15
 e = 0.1
 
-with open("rt-polarity.pos") as file_positive:
-    all_text_positive_file = file_positive.read()
+with open("rt-polarity.pos", "r") as file_positive:
+    all_text_positive_file = file_positive.readlines()[0:5000]
+    i = 0
+    while i < 5000:
+        new_positive_lines.append(all_text_positive_file[i].split())
+        new_positive_lines[i].insert(0, "<$>")
+        new_positive_lines[i].insert(len(new_positive_lines[i]), "</$>")
+        i += 1
 with open("rt-polarity.neg", "r") as file_negative:
-    all_text_negative_file = file_negative.read()
+    all_text_negative_file = file_negative.readlines()[0:5000]
+    i = 0
+    while i < 5000:
+        new_negative_lines.append(all_text_negative_file[i].split())
+        new_negative_lines[i].insert(0, "<$>")
+        new_negative_lines[i].insert(len(new_negative_lines[i]), "</$>")
+        i += 1
 
 
-def clean_text(text: str):
-    text = text.replace('*', '')
-    text = text.replace('.', '')
-    text = text.replace(',', '')
-    text = text.replace('?', '')
-    text = text.replace('!', '')
-    text = text.replace(':', '')
-    text = text.replace('[', '')
-    text = text.replace(']', '')
-    text = text.replace('"', '')
-    text = text.replace('-', " ")
-    text = text.replace("  ", " ")
-    return text
+def get_key(val, dictionary: dict):
+    for my_key, my_value in dictionary.items():
+        if val == my_value:
+            return my_key
+
+
+def clean_lines():
+    global new_positive_lines, new_negative_lines
+    tmp_list = []
+
+    for i in new_positive_lines:
+        for j in i:
+            if j == '*' or j == '.' or j == ',' or j == '?' or j == '!' or j == ':' or j == '"' or j == '[' or j == ']':
+                pass
+            else:
+                tmp_list.append(j)
+    new_positive_lines = tmp_list.copy()
+
+    tmp_list.clear()
+    for i in new_negative_lines:
+        for j in i:
+            if j == '*' or j == '.' or j == ',' or j == '?' or j == '!' or j == ':' or j == '"' or j == '[' or j == ']':
+                pass
+            else:
+                tmp_list.append(j)
+    new_negative_lines = tmp_list.copy()
 
 
 def create_both_hashmap():
-    for line in positive_lines:
-        for word in line.split():
-            if word not in positive_hashmap:
-                positive_hashmap[word] = 1
-            else:
-                positive_hashmap[word] += 1
+    for word in new_positive_lines:
+        if word not in positive_hashmap:
+            positive_hashmap[word] = 1
+        else:
+            positive_hashmap[word] += 1
 
-    for line in negative_lines:
-        for word in line.split():
-            if word not in negative_hashmap:
-                negative_hashmap[word] = 1
-            else:
-                negative_hashmap[word] += 1
+    for word in new_negative_lines:
+        if word not in negative_hashmap:
+            negative_hashmap[word] = 1
+        else:
+            negative_hashmap[word] += 1
 
 
 def clean_both_hashmap():
@@ -61,10 +86,15 @@ def clean_both_hashmap():
     # save begin and end of file
 
     for iterator in range(number_of_maximum_values):
-        key = max(positive_hashmap, key=positive_hashmap.get)
+        # [-3] is for <$> and </$>
+        tmp = positive_hashmap.copy()
+        value = sorted(tmp.values())[-3]
+        key = get_key(value, tmp)
         deleted_words_positive[key] = positive_hashmap.pop(key)
 
-        key = max(negative_hashmap, key=negative_hashmap.get)
+        tmp = negative_hashmap.copy()
+        value = sorted(tmp.values())[-3]
+        key = get_key(value, tmp)
         deleted_words_negative[key] = negative_hashmap.pop(key)
 
     # print("length of positive_hashmap before cleaning:", len(positive_hashmap))
@@ -93,16 +123,29 @@ def clean_both_hashmap():
     """""
 
 
-def update_and_clean_lines():
-    for i in deleted_words_positive:
-        count = positive_lines_word_by_word.count(i)
-        for j in range(count):
-            positive_lines_word_by_word.remove(i)
-
-    for i in deleted_words_negative:
-        count = negative_lines_word_by_word.count(i)
-        for j in range(count):
-            negative_lines_word_by_word.remove(i)
+#
+# def update_and_clean_lines():
+#     for i in deleted_words_positive:
+#         count = positive_lines_word_by_word.count(i)
+#         for j in range(count):
+#             positive_lines_word_by_word.remove(i)
+#
+#     for i in deleted_words_negative:
+#         count = negative_lines_word_by_word.count(i)
+#         for j in range(count):
+#             negative_lines_word_by_word.remove(i)
+def clean_lines_after_hashmap():
+    global new_positive_lines, new_negative_lines
+    tmp = []
+    for word in new_positive_lines:
+        if word in positive_hashmap:
+            tmp.append(word)
+    new_positive_lines = tmp.copy()
+    tmp.clear()
+    for word in new_negative_lines:
+        if word in negative_hashmap:
+            tmp.append(word)
+    new_negative_lines = tmp.copy()
 
 
 def calculate_probabilities_with_lambda():
@@ -125,44 +168,23 @@ def calculate_probabilities_with_lambda():
                                                   (lambda2 * p_wi_in_negative[key.split()[0]]) + (lambda1 * e)
 
 
+clean_lines()
+
 input_string = input()
 while input_string != "!q":
-    all_text_positive_file = clean_text(all_text_positive_file)
-    all_text_negative_file = clean_text(all_text_negative_file)
-
-    positive_lines = all_text_positive_file.splitlines()
-    negative_lines = all_text_negative_file.splitlines()
-
     create_both_hashmap()
     clean_both_hashmap()
-
-    # add <$> at the first of line and </$> at the end of line
-    i = 0
-    while i < len(positive_lines):
-        positive_lines[i] = "<$> " + positive_lines[i] + " </$>"
-        i += 1
-    # add to hashmap
-    positive_hashmap["<$>"] = i
-    positive_hashmap["</$>"] = i
-
-    i = 0
-    while i < len(negative_lines):
-        negative_lines[i] = "<$> " + negative_lines[i] + " </$>"
-        i += 1
-    # add to hashmap
-    negative_hashmap["<$>"] = i
-    negative_hashmap["</$>"] = i
+    # clean the lines that have stop words or repeat lower than 2
+    clean_lines_after_hashmap()
 
     # probability of each word in language
     p_wi_in_positive = {}
-    number_of_words_positive = sum(positive_hashmap.values())
     for key in positive_hashmap:
-        p_wi_in_positive[key] = positive_hashmap[key] / number_of_words_positive
+        p_wi_in_positive[key] = positive_hashmap[key] / len(new_positive_lines)
 
     p_wi_in_negative = {}
-    number_of_words_negative = sum(negative_hashmap.values())
     for key in negative_hashmap:
-        p_wi_in_negative[key] = negative_hashmap[key] / number_of_words_negative
+        p_wi_in_negative[key] = negative_hashmap[key] / len(new_negative_lines)
 
     positive_lines_word_by_word = str(positive_lines).split()
     negative_lines_word_by_word = str(negative_lines).split()
